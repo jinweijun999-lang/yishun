82e3ce66f7fc91202ad3b0d53f34ef24c3111d3f
Jarvanbro <xiajarvan@JarvanbrodeMac-mini-3.local>
2026-04-24 23:36:53 +0800

    fix: remove duplicate pluginManagement block with old Kotlin version

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,12 +15,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
-    plugins {
-        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-        id("com.android.application") version "8.2.0"
-        id("org.jetbrains.kotlin.android") version "1.9.22"
-    }
 }
 
 plugins {
